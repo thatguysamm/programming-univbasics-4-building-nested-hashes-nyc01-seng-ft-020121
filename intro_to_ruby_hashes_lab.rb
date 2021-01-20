b59ef5dc3c1fdd80163679a4e33db87ad58cc0bc
friendly-format-0074 <samuelagasi@gmail.com>
2021-01-20 19:57:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,12 @@ def monopoly_with_third_tier
     three_pieces_owned: 100,
     four_pieces_owned: 200}
 
-
+    names: {
+      reading_railroad: {},
+      pennsylvania_railroad: {},
+      b_and_o_railroad: {},
+      shortline_railroad: {},
+    }
   }
   }
 
